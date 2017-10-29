a7fdae941b821174ff892f90ca851020f6d386c1
jpic <jamespic@gmail.com>
2017-10-29 19:35:12 +0100

    Style proposal

--- src/crudlfap_example/settings.py
+++ src/crudlfap_example/settings.py
@@ -14,8 +14,49 @@ import os
 import importlib
 
 
-def add_optional_dep(module: str, to: list=None,
-                     before: str=None, after: str=None):
+def dep_importable(module):
+    try:
+        importlib.__import__(module)
+    except ModuleNotFoundError:
+        mod_path, dot, cls = module.rpartition('.')
+
+        if not mod_path:
+            return False
+
+        try:
+            mod = importlib.import_module(mod_path)
+        except ModuleNotFoundError:
+            return False
+        else:
+            if hasattr(mod, cls):
+                return True
+    else:
+        return True
+
+    return False
+
+
+def dep_insert_to(module, to, before, after):
+    if to is None:
+        to = INSTALLED_APPS
+
+    if not before and not after:
+        to.append(module)
+        return
+
+    try:
+        if before:
+            pos = to.index(before)
+        else:
+            pos = to.index(after) + 1
+    except ValueError:
+        pass
+    else:
+        to.insert(pos, module)
+
+
+def dep_optional_to(module: str, to: list=None,
+                    before: str=None, after: str=None):
     """
     Adds an optional dependency
 
@@ -31,42 +72,10 @@ def add_optional_dep(module: str, to: list=None,
     :param before: module string as should be available in the to list.
     :param after: module string as should be available in the to list.
     """
-    has_module = False
-    if to is None:
-        to = INSTALLED_APPS
-
-    try:
-        importlib.__import__(module)
-    except ModuleNotFoundError:
-        mod_path, dot, cls = module.rpartition('.')
-        if not mod_path:
-            return
-        try:
-            mod = importlib.import_module(mod_path)
-        except ModuleNotFoundError:
-            return
-        else:
-            if hasattr(mod, cls):
-                has_module = True
-    else:
-        has_module = True
-
-    if not has_module:
+    if not dep_importable(module):
         return
 
-    if not before and not after:
-        to.append(module)
-        return
-
-    try:
-        if before:
-            pos = to.index(before)
-        else:
-            pos = to.index(after) + 1
-    except ValueError:
-        pass
-    else:
-        to.insert(pos, module)
+    dep_insert_to(module, to, before, after)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -105,12 +114,12 @@ INSTALLED_APPS = [
 ]
 
 # CRUDLFA+ optional dependencies
-add_optional_dep('debug_toolbar', after='django.contrib.staticfiles')
-add_optional_dep('crudlfap_filtertables2', before='crudlfap_example.artist')
-add_optional_dep('django_filters', before='crudlfap_example.artist')
-add_optional_dep('django_tables2', before='crudlfap_example.artist')
-add_optional_dep('dal', before='crudlfap_example.artist')
-add_optional_dep('dal_select2', before='crudlfap_example.artist')
+dep_optional_to('debug_toolbar', after='django.contrib.staticfiles')
+dep_optional_to('crudlfap_filtertables2', before='crudlfap_example.artist')
+dep_optional_to('django_filters', before='crudlfap_example.artist')
+dep_optional_to('django_tables2', before='crudlfap_example.artist')
+dep_optional_to('dal', before='crudlfap_example.artist')
+dep_optional_to('dal_select2', before='crudlfap_example.artist')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,8 +130,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-add_optional_dep('debug_toolbar.middleware.DebugToolbarMiddleware',
-                 to=MIDDLEWARE)
+dep_optional_to('debug_toolbar.middleware.DebugToolbarMiddleware',
+                to=MIDDLEWARE)
 
 INTERNAL_IPS = ('127.0.0.1',)
 
