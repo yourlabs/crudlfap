7c8e6db83d464915270002dab8c2232df631520e
Melvyn Sopacua <melvyn@pluggable.nl>
2017-10-30 20:30:43 +0100

    Why is this not in core?
    IONO

--- src/crudlfap_example/manage.py
+++ src/crudlfap_example/manage.py
@@ -9,6 +9,25 @@ def main():
         "crudlfap_example.settings"
     )
     from django.core.management import execute_from_command_line
+    if sys.argv[1] == 'shell' and len(sys.argv) == 2:
+        import readline
+        import atexit
+        histfile = os.path.join(os.path.expanduser("~"), ".django_history")
+
+        try:
+            readline.read_history_file(histfile)
+            h_len = readline.get_current_history_length()
+        except FileNotFoundError:
+            open(histfile, 'wb').close()
+            h_len = 0
+
+        def save(prev_h_len, fname):
+            new_h_len = readline.get_current_history_length()
+            readline.set_history_length(1000)
+            readline.append_history_file(new_h_len - prev_h_len, fname)
+
+        atexit.register(save, h_len, histfile)
+
     execute_from_command_line(sys.argv)
 
 if __name__ == "__main__":
