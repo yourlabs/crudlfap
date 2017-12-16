f7a6dce027faad4ab3bfa79132a8cf28b9bd3766
jpic <jamespic@gmail.com>
2017-12-16 07:28:45 +0100

    Hookable and unattended dev command

--- src/crudlfap/management/commands/dev.py
+++ src/crudlfap/management/commands/dev.py
@@ -1,17 +1,62 @@
+import os
+import os.path
+import secrets
+import string
+import sys
+
+import django.dispatch
+from django.apps import apps
+from django.conf import settings
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.commands.runserver import Command
+from django.utils.timezone import now
+
+signal = django.dispatch.Signal()
 
 
-class Command(BaseCommand):
-    help = 'Migrate/Createsuperuser/Runserver'
+def rnpw(num=28):
+    return ''.join(secrets.choice(
+        string.ascii_uppercase + string.digits) for _ in range(num))
+
+
+class Command(Command):
+    help = 'Start development environment'
 
     def handle(self, *args, **options):
         call_command('migrate')
+        self.createusers()
+        self.runserver(*args, **options)
 
-        from django.conf import settings
-        from django.apps import apps
+    def runserver(self, *args, **options):
+        try:
+            pid = os.fork()
+        except OSError:
+            sys.exit(1)
+
+        if pid == 0:
+            signal.send(sender=self)
+        else:
+            return super().handle(*args, **options)
+
+    def createusers(self):
         user_model = apps.get_model(settings.AUTH_USER_MODEL)
-        if not user_model.objects.count():
-            call_command('createsuperuser')
 
-        call_command('runserver')
+        def createuser(username, **defaults):
+            user, created = user_model.objects.update_or_create(
+                username=username,
+                defaults=defaults,
+            )
+
+            if user.last_login:
+                last_login = now() - user.last_login
+                if last_login.seconds >= 3600:
+                    password = rnpw()
+                    user.set_password(password)
+                    user.save()
+                    print('Login with {} / {}'.format(username, password))
+
+            return user, created
+
+        createuser('dev', is_staff=True, is_superuser=True)
+        createuser('staff', is_staff=True)
+        createuser('user')
