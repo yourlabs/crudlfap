9d4e0ce956c14d3cf30a429a96feba8b0ee5283b
jpic <jamespic@gmail.com>
2018-04-09 13:13:07 +0200

    Fix conflict with contrib.sites middleware

--- src/crudlfap/settings.py
+++ src/crudlfap/settings.py
@@ -113,7 +113,7 @@ CRUDLFAP_TEMPLATE_BACKEND = {
         "constants": TEMPLATE_CONSTANTS,
         "globals": {
             "pagination_filter_params": "crudlfap.jinja2.pagination_filter_params",  # noqa
-            "site": "crudlfap.crudlfap.site",
+            "crudlfap_site": "crudlfap.crudlfap.site",
             "getattr": getattr,
             "str": str,
             "int": int,
