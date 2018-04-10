697316ce1f39c2d4322e2dc5b07556046522969d
jpic <jamespic@gmail.com>
2018-04-10 20:17:15 +0200

    More cleaning in tables code

--- src/crudlfap_filtertables2/jinja2.py
+++ src/crudlfap_filtertables2/jinja2.py
@@ -9,7 +9,7 @@ def render_table(request, table, object_list):
         object_list=object_list
     ))
     template = Template('''
-    {% load django_tables2 %}{% render_table table "django_tables2/bootstrap.html" %}
+    {% load django_tables2 %}{% render_table table %}
     ''')
     html = template.render(context)
     return html
