23d53a13faa51b52ce07a5b43607c2847a173eac
jpic <jamespic@gmail.com>
2018-04-15 20:03:48 +0200

    Security tests based on get_objects_for_user

--- src/crudlfap_example/song/crudlfap.py
+++ src/crudlfap_example/song/crudlfap.py
@@ -37,4 +37,19 @@ class SongRouter(crudlfap.Router):
         ),
     ]
 
+    def allowed(self, view):
+        # In this example, we let users do everything on their own objects
+        # and that's taken care of by get_objects_for_user. So, we short-
+        # circuit the Django permission system which takes place by default
+        return True
+
+    def get_objects_for_user(self, user, perms):
+        if not user.is_authenticated:
+            return self.model.objects.none()
+
+        if user.is_staff or user.is_superuser:
+            return self.model.objects.all()
+
+        return self.model.objects.filter(owner=user)
+
 SongRouter().register()
