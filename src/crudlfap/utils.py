0f78302ab408c82578ed07be274a7c8a8466c196
jpic <jamespic@gmail.com>
2018-04-16 02:01:56 +0200

    Remove unused function

--- src/crudlfap/utils.py
+++ src/crudlfap/utils.py
@@ -1,16 +1,4 @@
 from django.core.exceptions import FieldDoesNotExist
-from django.urls.base import get_resolver, get_urlconf
-
-
-def view_resolve(url_name, urlconf=None):
-    if urlconf is None:
-        urlconf = get_urlconf()
-
-    resolver = get_resolver(urlconf)
-
-    for url in resolver.url_patterns:
-        if url.name == url_name:
-            return url.callback.view_class
 
 
 def guess_urlfield(model):
