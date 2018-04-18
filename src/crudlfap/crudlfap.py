d86b360750a3f3844ef1cbfe31803e309e6e5134
jpic <jamespic@gmail.com>
2018-04-18 15:21:45 +0200

    Syntactic sugar around .views

--- src/crudlfap/crudlfap.py
+++ src/crudlfap/crudlfap.py
@@ -6,7 +6,7 @@ from .apps import _installed
 from .factory import Factory
 from .registry import Registry
 from .route import Route
-from .router import Router
+from .router import Router, Views, ViewsDescriptor
 from .site import site
 from .views.debug import UrlsView
 from .views.generic import (
