e5addd99d3a7a48644c3dad5f85b71e177917338
navjot.g <navjot.g@cisinlabs.com>
2018-04-20 15:51:02 +0530

    Add sauce-tunnel package Add start tunnel code on gloabal.js file under before and after callback update caonfig.yml file under circleci

--- nightwatch/globals.js
+++ nightwatch/globals.js
@@ -1,14 +1,27 @@
 const chromedriver = require('chromedriver');
+const sauceTunnel = require('sauce-tunnel');
+
+const tunnel = new sauceTunnel('navjot.g', 'f817b186-0804-45a3-bdff-597eafc9da71', 'tunnel', true/* ['--verbose'] */);
+
 module.exports = {
     before: (done) => {
-        chromedriver.start();
-
-        done();
+        console.log('------chromedriver---start');
+        tunnel.start(function (status) {
+            if (status === false) {
+                throw new Error('Something went wrong with the tunnel');
+            }
+            console.log('------tunnel---start');
+            chromedriver.start();
+            done();
+        });
     },
-
     after: (done) => {
-        chromedriver.stop();
-
-        done();
+        console.log('------chromedriver---stop');
+        tunnel.stop(function () {
+            // Tunnel destroyed
+            console.log('------tunnel---stop');
+            chromedriver.stop();
+            done();
+        });
     }
 };
