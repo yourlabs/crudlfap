15025f9d3fbf6912d062e76f8ab7848e7e26dec7
jpic <jamespic@gmail.com>
2018-04-21 22:29:22 +0200

    Use Dockerfile USER instead of su, refactor browser test

--- src/crudlfap/settings.py
+++ src/crudlfap/settings.py
@@ -16,8 +16,8 @@ from crudlfap.conf import install_optional  # noqa
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
