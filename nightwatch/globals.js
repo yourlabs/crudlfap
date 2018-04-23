c4ec6ef951d7b4ae8b57943773c4c109cf9750f3
navjot.g <navjot.g@cisinlabs.com>
2018-04-23 20:19:55 +0530

    Linting work

--- nightwatch/globals.js
+++ nightwatch/globals.js
@@ -1,45 +1,27 @@
-// const chromedriver = require('chromedriver');
-// const sauceTunnel = require('sauce-tunnel');
-const sauceConnectLauncher = require('sauce-connect-launcher')
-// const tunnel = new sauceTunnel('navjot.g', 'f817b186-0804-45a3-bdff-597eafc9da71', 'tunnel', true/* ['--verbose'] */);
-let sauceConnectProcess1;
+const sauceConnectLauncher = require('sauce-connect-launcher');
+
+let sauceConnectTunnel;
+
 module.exports = {
-    before: (done) => {
-        console.log('------chromedriver---start');
+  before: (done) => {
+    sauceConnectLauncher({
+      username: 'navjot.g',
+      accessKey: 'f817b186-0804-45a3-bdff-597eafc9da71'
+    }, function (err, sauceConnectProcess) {
+      if (err) {
+        console.error(err.message);
 
-        // tunnel.start(function (status) {
-        //     if (status === false) {
-        //         throw new Error('Something went wrong with the tunnel');
-        //     }
-        //     console.log('------tunnel---start');
-        //     // chromedriver.start();
-        //     done();
-        // });
-        sauceConnectLauncher({
-            username: 'navjot.g',
-            accessKey: 'f817b186-0804-45a3-bdff-597eafc9da71'
-        }, function (err, sauceConnectProcess) {
-            if (err) {
-                console.error(err.message);
-                return;
-            }
-            console.log("Sauce Connect ready");
-            sauceConnectProcess1 = sauceConnectProcess;
-            done();
-        });
-    },
-    after: (done) => {
-        console.log('------chromedriver---stop');
-        // tunnel.stop(function () {
-        //     // Tunnel destroyed
-        //     console.log('------tunnel---stop');
-        //     // chromedriver.stop();
-        //     done();
-        // });
-
-        sauceConnectProcess1.close(function () {
-            console.log("Closed Sauce Connect process");
-            done();
-        })
-    }
+        return;
+      }
+      console.log('Sauce Connect ready');
+      sauceConnectTunnel = sauceConnectProcess;
+      done();
+    });
+  },
+  after: (done) => {
+    sauceConnectTunnel.close(function () {
+      console.log('Closed Sauce Connect process');
+      done();
+    });
+  }
 };
