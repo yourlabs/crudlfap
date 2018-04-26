2a33e72f4dacb3fd05d496eaf3a6df769e7d794a
jpic <jamespic@gmail.com>
2018-04-27 01:15:54 +0200

    v0.3.28

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='crudlfap',
-    version='0.3.24',
+    version='0.3.28',
     description='Rich frontend for generic views with Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
