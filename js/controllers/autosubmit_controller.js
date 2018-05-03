00d51b35f7f042da9cc0cf13ffd7acb3ca1e2371
jpic <jamespic@gmail.com>
2018-05-03 09:33:32 +0200

    Fugbix: bind content in dynamic list table

--- js/controllers/autosubmit_controller.js
+++ js/controllers/autosubmit_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from 'stimulus'
 import serialize from 'form-serialize'
+import init from '../init.js'
 
 export default class extends Controller {
   input() {
@@ -26,7 +27,9 @@ export default class extends Controller {
   onload(e) {
     var parser = new DOMParser()
     var doc = parser.parseFromString(e.target.responseText, 'text/html')
-    document.getElementById(this.targetId).innerHTML = doc.getElementById(this.targetId).innerHTML
+    var target = document.getElementById(this.targetId)
+    target.innerHTML = doc.getElementById(this.targetId).innerHTML
+    init(target)
     window.history.pushState({}, doc.querySelector('title').innerHTML, this.url)
   }
 
