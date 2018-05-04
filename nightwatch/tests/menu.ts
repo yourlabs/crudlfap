4ad8b697c1ddba3ec1cf652aa78792b193317583
navjot.g <navjot.g@cisinlabs.com>
2018-05-04 18:25:57 +0530

    Solved test cases for IE

--- nightwatch/tests/menu.ts
+++ nightwatch/tests/menu.ts
@@ -1,44 +1,51 @@
 import { NightwatchBrowser } from 'nightwatch';
-import {CONSTANTS} from './../shared/CONSTANTS';
+import { CONSTANTS } from './../shared/CONSTANTS';
 module.exports = {
-    // before: (browser: NightwatchBrowser) => {
-    //     browser
-    //         .url(CONSTANTS.BASE_URL)
-    //         .waitForElementVisible('body', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
-    // },
     'Before Login: Menu should visible when click on the menu icon': (browser: NightwatchBrowser) => {
         browser
             .url(CONSTANTS.BASE_URL)
             .waitForElementVisible('body', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
-            .assert.visible('span[data-action="click->sidenav#click"]')
-            .isVisible('ul[id="slide-out"]', (result) => {
-                browser.assert.equal(result.value, false)
+
+            .assert.visible('span[class="sidenav-trigger"]')
+            .element('id', 'slide-out', function (result) {
+                browser.assert.equal(result.status, false)
             })
-            .click('span[data-action="click->sidenav#click"]')
+            .click('span[class="sidenav-trigger"]')
             .waitForElementVisible('ul[id="slide-out"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
             .isVisible('ul[id="slide-out"]', (result) => {
                 browser.assert.equal(result.value, true)
             })
             .click('ul#slide-out li.no-padding a[href="/"]')
+            .end();
     },
 
     'Before Login: Menu should contains "login" option': (browser: NightwatchBrowser) => {
         browser
-            .waitForElementVisible('span[data-action="click->sidenav#click"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
-        
-            .assert.visible('span[data-action="click->sidenav#click"]')
-            .click('span[data-action="click->sidenav#click"]')
+            .url(CONSTANTS.BASE_URL)
+            .waitForElementVisible('body', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
+            .waitForElementVisible('span[class="sidenav-trigger"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
+
+            .assert.visible('span[class="sidenav-trigger"]')
+            .click('span[class="sidenav-trigger"]')
             .waitForElementVisible('ul#slide-out li.no-padding a[href="/login"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
 
             .elements('css selector', 'ul#slide-out li.no-padding a[href="/login"]', (result) => {
                 browser.assert.equal(result.value.length, 1)
-            });
+            })
+            .end();
     },
 
     'After Login: Menu should contains the "logout" option': (browser: NightwatchBrowser) => {
         browser
-            .assert.visible('ul#slide-out li.no-padding a[href="/login"]')
+            .url(CONSTANTS.BASE_URL)
+            .waitForElementVisible('body', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
+
+            .elements('css selector', 'ul#slide-out li.no-padding a[href="/login"]', (result) => {
+                browser.assert.equal(result.value.length, 1)
+            })
+
             .click('ul#slide-out li.no-padding a[href="/login"]')
+
             .waitForElementVisible('input[id=id_username]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
             .assert.visible('input[id=id_username]')
             .setValue('input[id=id_username]', CONSTANTS.USER_CREDENTIALS.RIGHT.USERNAME)
@@ -49,9 +56,8 @@ module.exports = {
             .click('button[type=submit]')
             .waitForElementVisible('ul[class="right"] li a[href="/logout"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
 
-        browser
-            .assert.visible('span[data-action="click->sidenav#click"]')
-            .click('span[data-action="click->sidenav#click"]')
+            .assert.visible('span[class="sidenav-trigger"]')
+            .click('span[class="sidenav-trigger"]')
             .waitForElementVisible('ul[id="slide-out"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
             .assert.visible('ul[id="slide-out"]')
 
@@ -60,14 +66,17 @@ module.exports = {
             })
             .click('ul#slide-out li.no-padding a[href="/logout"]')
             .waitForElementVisible('ul[class="right"] li a[href="/login"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
+            .end();
     },
     'After Logout: Menu should contains "login" option': (browser: NightwatchBrowser) => {
         browser
-            .assert.visible('span[data-action="click->sidenav#click"]')
-            .isVisible('ul[id="slide-out"]', (result) => {
-                browser.assert.equal(result.value, false)
+            .url(CONSTANTS.BASE_URL)
+            .waitForElementVisible('body', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
+            .assert.visible('span[class="sidenav-trigger"]')
+            .element('id', 'slide-out', function (result) {
+                browser.assert.equal(result.status, false)
             })
-            .click('span[data-action="click->sidenav#click"]')
+            .click('span[class="sidenav-trigger"]')
             .waitForElementVisible('ul[class="right"] li a[href="/login"]', CONSTANTS.WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT)
             .isVisible('ul[id="slide-out"]', (result) => {
                 browser.assert.equal(result.value, true)
