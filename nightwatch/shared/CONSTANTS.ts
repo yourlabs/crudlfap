41ae229f74b063798366265ab941aa1814f02ed5
navjot.g <navjot.g@cisinlabs.com>
2018-05-07 20:35:40 +0530

    Write some new test cases for Group functionlity

--- nightwatch/shared/CONSTANTS.ts
+++ nightwatch/shared/CONSTANTS.ts
@@ -1,5 +1,10 @@
 export const CONSTANTS = {
     BASE_URL: 'http://localhost:8000',
+    GROUP: {
+        CREATE: 'http://localhost:8000/group/create',
+        LIST: 'http://localhost:8000/group',
+        INPUT: 'Test Group for sample'
+    },
     WAIT_FOR_ELEMENT_VISIBLE_TIMEOUT: 3000,
     PAUSE_TIMEOUT: 3000,
     USER_CREDENTIALS: {
