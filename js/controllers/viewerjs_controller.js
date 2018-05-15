21ab078eeed3cc423d8ace6f883027650d0a03a6
jpic <jamespic@gmail.com>
2018-05-15 18:16:58 +0200

    Update ViewerJS controller

--- js/controllers/viewerjs_controller.js
+++ js/controllers/viewerjs_controller.js
@@ -4,13 +4,52 @@ import Viewer from 'viewerjs'
 export default class extends Controller {
   click(e) {
     e.preventDefault()
-
-    if (!this.viewer) {
-      this.viewer = new Viewer(document.getElementById(this.element.getAttribute('data-viewerjs')), {
-        url: 'data-original',
-      })
-    }
-
     this.viewer.show()
+  }
+
+  get images() {
+    return document.querySelectorAll('[data-viewerjs=' + this.viewerName + ']')
+  }
+
+  get viewerName() {
+    return this.element.getAttribute('data-viewerjs')
+  }
+
+  get viewerId() {
+    return 'viewerjs-' + this.viewerName
+  }
+
+  get viewerElement() {
+    var viewer = document.getElementById(this.viewerId)
+    if (!viewer) {
+      // no web component for me, damn you ie
+      viewer = document.createElement('ul')
+      viewer.setAttribute('id', this.viewerId)
+      viewer.style.display = 'none'
+      for (var image of this.images) {
+        var img = document.createElement('img')
+        img.setAttribute('data-original', image.getAttribute('href'))
+        img.setAttribute('src', image.getAttribute('href'))
+        img.setAttribute('alt', image.text)
+        var item = document.createElement('li')
+        item.appendChild(img)
+        viewer.appendChild(item)
+      }
+      document.body.appendChild(viewer)
+    }
+    return viewer
+  }
+
+  get viewerOptions() {
+    return {
+      url: 'data-original',
+    }
+  }
+
+  get viewer() {
+    if (this.viewerObject === undefined) {
+      this.viewerObject = new Viewer(this.viewerElement, this.viewerOptions)
+    }
+    return this.viewerObject
   }
 }
