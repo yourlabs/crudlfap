bac3c0fb8728801ece6405062a91374716d6c230
jpic <jamespic@gmail.com>
2018-05-17 19:15:05 +0200

    Fix browser tests

--- src/crudlfap/management/commands/dev.py
+++ src/crudlfap/management/commands/dev.py
@@ -8,7 +8,8 @@ import django.dispatch
 from django.apps import apps
 from django.conf import settings
 from django.core.management import call_command
-from django.core.management.base import BaseCommand
+from django.core.management.commands.runserver import Command as BaseCommand
+
 
 try:
     import devpy.develop as logger
