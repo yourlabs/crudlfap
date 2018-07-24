c650034f73cfd507b250a23af7936b295b708b71
jpic <jamespic@gmail.com>
2018-07-24 05:57:30 +0200

    Add preloader

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ import './style.sass'
   if (window.Turbolinks === undefined) {
     var Turbolinks = require('turbolinks')
     Turbolinks.start()
+    Turbolinks.setProgressBarDelay(250)
   }
 
   // support to IE
@@ -64,36 +65,45 @@ document.addEventListener('click', function(e) {
   }
 })
 
-const fetchers = {};
+const fetchers = {}
 
 function prefetchCleanup(event) {
-  clearTimeout(fetchers[event.target.href]);
-  event.target.removeEventListener('mouseleave', prefetchCleanup);
+  clearTimeout(fetchers[event.target.href])
+  event.target.removeEventListener('mouseleave', prefetchCleanup)
 }
 
 // link preload on hover poc by @hopsoft
 // https://github.com/turbolinks/turbolinks/issues/313#issuecomment-407108379
 document.addEventListener('mouseover', event => {
-  let url = event.target.href;
-  if (!event.target.href) return;
-  clearTimeout(fetchers[url]);
-  if (Turbolinks.controller.cache.has(url)) return;
+  let url = event.target.href
+  if (!event.target.href) return
+  clearTimeout(fetchers[url])
+  if (window.Turbolinks.controller.cache.has(url)) return
+  if (event.target.dataset.noprefetch) return
 
-  event.target.addEventListener('mouseleave', prefetchCleanup);
+
+  event.target.addEventListener('mouseleave', prefetchCleanup)
 
   fetchers[url] = setTimeout(() => {
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute('style', 'display:none');
-    iframe.setAttribute('src', url);
+    const iframe = document.createElement('iframe')
+    iframe.setAttribute('style', 'display:none')
+    iframe.setAttribute('src', url)
     iframe.addEventListener('load', () => {
-      Turbolinks.controller.cache.put(
+      window.Turbolinks.controller.cache.put(
         url,
-        Turbolinks.Snapshot.fromElement(iframe.contentDocument.documentElement)
-      );
-      iframe.parentElement.removeChild(iframe);
-    });
-    document.body.appendChild(iframe);
-  }, 250);
-});
+        window.Turbolinks.Snapshot.fromElement(iframe.contentDocument.documentElement)
+      )
+      iframe.parentElement.removeChild(iframe)
+    })
+    document.body.appendChild(iframe)
+  }, 250)
+})
+
+document.addEventListener('turbolinks:click', () => {
+  document.getElementById('main-loader').style.display = 'flex'
+})
+document.addEventListener('turbolinks:render', () => {
+  document.getElementById('main-loader').style.display = 'none'
+})
 
 export default application
