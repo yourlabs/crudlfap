80bf549d9cce9649195cf67a2961279b8cbcc844
jpic <jamespic@gmail.com>
2018-07-24 18:00:17 +0200

    Release 0.4.11

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='crudlfap',
-    version='0.4.9',
+    version='0.4.11',
     description='Rich frontend for generic views with Django',
     author='James Pic',
     author_email='jamespic@gmail.com',
