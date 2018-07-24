c7864ea13e349fb96676113b8ca550273676b655
jpic <jamespic@gmail.com>
2018-07-24 05:19:20 +0200

    Experiment with turbolinks prefetch after 250ms hover

--- js/index.js
+++ js/index.js
@@ -64,4 +64,36 @@ document.addEventListener('click', function(e) {
   }
 })
 
+const fetchers = {};
+
+function prefetchCleanup(event) {
+  clearTimeout(fetchers[event.target.href]);
+  event.target.removeEventListener('mouseleave', prefetchCleanup);
+}
+
+// link preload on hover poc by @hopsoft
+// https://github.com/turbolinks/turbolinks/issues/313#issuecomment-407108379
+document.addEventListener('mouseover', event => {
+  let url = event.target.href;
+  if (!event.target.href) return;
+  clearTimeout(fetchers[url]);
+  if (Turbolinks.controller.cache.has(url)) return;
+
+  event.target.addEventListener('mouseleave', prefetchCleanup);
+
+  fetchers[url] = setTimeout(() => {
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('style', 'display:none');
+    iframe.setAttribute('src', url);
+    iframe.addEventListener('load', () => {
+      Turbolinks.controller.cache.put(
+        url,
+        Turbolinks.Snapshot.fromElement(iframe.contentDocument.documentElement)
+      );
+      iframe.parentElement.removeChild(iframe);
+    });
+    document.body.appendChild(iframe);
+  }, 250);
+});
+
 export default application
