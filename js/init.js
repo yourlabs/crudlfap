bee6dc823cce0fb46f1e8f091fb85659c6d4af49
jpic <jamespic@gmail.com>
2018-07-31 01:49:32 +0200

    Only init el in init

--- js/init.js
+++ js/init.js
@@ -2,13 +2,13 @@ import M from 'materialize-css'
 
 export default function(el) {
   M.AutoInit(el)
-  var elems = document.querySelectorAll('.dropdown-trigger.no-autoinit')
+  var elems = el.querySelectorAll('.dropdown-trigger.no-autoinit')
   M.Dropdown.init(elems, {
     alignment: 'right',
     constrainWidth: false,
     coverTrigger: false,
   })
-  for (let element of document.querySelectorAll('.crudlfap-datepicker')) {
+  for (let element of el.querySelectorAll('.crudlfap-datepicker')) {
     M.Datepicker.init(element, {
       showClearBtn: element.getAttribute('data-clearable'),
       format: element.getAttribute('data-format'),
