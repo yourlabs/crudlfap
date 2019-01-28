5b0d6a9c5ceab04db8d2f48a9df41ae1d568de2d
jpic <jpic@yourlabs.org>
2019-01-28 12:25:11 +0100

    Improve case detection
    This improves compatibility with more standard has_perm calls.

--- src/crudlfap_auth/backends.py
+++ src/crudlfap_auth/backends.py
@@ -5,15 +5,24 @@ class ViewBackend:
         """
         return None
 
-    def has_perm(self, user_obj, perm, obj=None):
-        if not obj.authenticate:
-            return True
-
-        elif not user_obj.is_authenticated:
+    def has_perm(self, user_obj, perm, obj=None):  # noqa
+        try:
+            if not obj.authenticate:
+                return True
+        except AttributeError:
             return False
 
-        if user_obj.is_superuser or obj.allowed_groups == 'any':
+        if not user_obj.is_authenticated:
+            return False
+
+        if user_obj.is_superuser:
             return True
+
+        try:
+            if obj.allowed_groups == 'any':
+                return True
+        except AttributeError:
+            return False
 
         for group in user_obj.groups.all():
             if group.name in obj.allowed_groups:
