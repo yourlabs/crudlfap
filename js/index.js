f0f7f89594e54ddc3372318fc00896394e9bf16f
jpic <jpic@yourlabs.org>
2019-03-07 23:58:20 +0100

    Remove fetch preload poc

--- js/index.js
+++ js/index.js
@@ -86,33 +86,6 @@ function prefetchCleanup(event) {
   event.target.removeEventListener('mouseleave', prefetchCleanup)
 }
 
-// link preload on hover poc by @hopsoft
-// https://github.com/turbolinks/turbolinks/issues/313#issuecomment-407108379
-document.addEventListener('mouseover', event => {
-  let url = event.target.href
-  if (!event.target.href) return
-  clearTimeout(fetchers[url])
-  if (window.Turbolinks.controller.cache.has(url)) return
-  if (event.target.dataset.noprefetch) return
-
-
-  event.target.addEventListener('mouseleave', prefetchCleanup)
-
-  fetchers[url] = setTimeout(() => {
-    const iframe = document.createElement('iframe')
-    iframe.setAttribute('style', 'display:none')
-    iframe.setAttribute('src', url)
-    iframe.addEventListener('load', () => {
-      window.Turbolinks.controller.cache.put(
-        url,
-        window.Turbolinks.Snapshot.fromHTMLElement(iframe.contentDocument.documentElement)
-      )
-      iframe.parentElement.removeChild(iframe)
-    })
-    document.body.appendChild(iframe)
-  }, 250)
-})
-
 document.addEventListener('turbolinks:click', loader.show)
 document.addEventListener('turbolinks:render', loader.hide)
 
