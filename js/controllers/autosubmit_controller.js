b30b46143c83871f4f86d8cd74e17e6fb99861f7
jpic <jpic@yourlabs.org>
2019-03-21 15:02:40 +0100

    Fix navigation arrows on list view / autosubmit controller
    Use Turbolinks instead of reinventing it ... This is one of the first
    controllers iirc perhaps added before turbolinks (stimulus came first).
    
    Otherwise, well bad code is gone anyway ^^

--- js/controllers/autosubmit_controller.js
+++ js/controllers/autosubmit_controller.js
@@ -7,33 +7,6 @@ export default class extends Controller {
     var data = serialize(this.element)
     var url = window.location.pathname + '?' + data
 
-    if (this.url !== undefined) {
-      if (this.url === url) {
-        // console.log('aborted for same url', url);
-        return
-      }
-      if (this.xhr !== undefined && this.xhr.readyState < 4) {
-        this.xhr.abort()
-      }
-    }
-
-    this.url = url
-    this.xhr = new XMLHttpRequest()
-    this.xhr.open('GET', this.url)
-    this.xhr.onload = this.onload.bind(this)
-    this.xhr.send()
-  }
-
-  onload(e) {
-    var parser = new DOMParser()
-    var doc = parser.parseFromString(e.target.responseText, 'text/html')
-    var target = document.getElementById(this.targetId)
-    target.innerHTML = doc.getElementById(this.targetId).innerHTML
-    init(target)
-    window.history.pushState({}, doc.querySelector('title').innerHTML, this.url)
-  }
-
-  get targetId() {
-    return this.element.getAttribute('data-target')
+    Turbolinks.visit(url)
   }
 }
