966322774da7aba029ac159fc3b92b313b0e4523
jpic <jpic@yourlabs.org>
2019-06-20 01:02:50 +0200

    Enforce static file serve in crudlfap_example
    And help reduce demo effects xD

--- src/crudlfap_example/urls.py
+++ src/crudlfap_example/urls.py
@@ -1,10 +1,19 @@
+import re
+
 from crudlfap import shortcuts as crudlfap
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path
+from django.views.static import serve
 
-urlpatterns = [crudlfap.site.urlpattern]
-
+urlpatterns = [
+    crudlfap.site.urlpattern,
+    re_path(
+        r'^%s(?P<path>.*)$' % settings.STATIC_URL.lstrip('/'),
+        serve,
+        kwargs=dict(document_root=settings.STATIC_ROOT)
+    ),
+]
 if 'debug_toolbar' in settings.INSTALLED_APPS and settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
