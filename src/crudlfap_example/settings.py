8127d09fff5ca9f28da9ba5e72b82054c2f5bf95
jpic <jpic@yourlabs.org>
2021-04-21 09:49:53 +0200

    Make settings more generic

--- src/crudlfap_example/settings.py
+++ src/crudlfap_example/settings.py
@@ -11,6 +11,18 @@ INSTALLED_APPS += [  # noqa
     'crudlfap_registration',
 ]
 
+ROOT_URLCONF = 'crudlfap_example.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'HOST': os.getenv('DB_HOST'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+    }
+}
+
 install_optional(OPTIONAL_APPS, INSTALLED_APPS)  # noqa
 install_optional(OPTIONAL_MIDDLEWARE, MIDDLEWARE)  # noqa
 
