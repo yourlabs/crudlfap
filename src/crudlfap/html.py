c2ca64d7d40a7c7c1d4d780b30dbcee216faa8e1
jpic <jpic@yourlabs.org>
2021-05-13 21:42:25 +0200

    Add a spinner on slow requests

--- src/crudlfap/html.py
+++ src/crudlfap/html.py
@@ -159,6 +159,91 @@ class Body(Body):
             up.log.enable()
 
 
+class SpinnerOverlay(Div):
+    sass = '''
+    .SpinnerOverlay
+        position: fixed
+        width: 100%
+        height: 100%
+        top: 0
+        left: 0
+        right: 0
+        bottom: 0
+        background-color: rgba(255, 255, 255, 0.66)
+        z-index: 20000
+        cursor: pointer
+        padding-top: 30vh
+
+        .loader
+            color: var(--mdc-theme-primary)
+            opacity: 50%
+            font-size: 90px
+            text-indent: -9999em
+            overflow: hidden
+            width: 1em
+            height: 1em
+            border-radius: 50%
+            margin: 72px auto
+            position: relative
+            -webkit-transform: translateZ(0)
+            -ms-transform: translateZ(0)
+            transform: translateZ(0)
+            -webkit-animation: load6 1.7s infinite ease, round 1.7s infinite ease
+            animation: load6 1.7s infinite ease, round 1.7s infinite ease
+
+        @-webkit-keyframes load6
+            0%
+                box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em
+            5%, 95%
+                box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em
+            10%, 59%
+                box-shadow: 0 -0.83em 0 -0.4em, -0.087em -0.825em 0 -0.42em, -0.173em -0.812em 0 -0.44em, -0.256em -0.789em 0 -0.46em, -0.297em -0.775em 0 -0.477em
+            20%
+                box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em, -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em, -0.749em -0.34em 0 -0.477em
+            38%
+                box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em, -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em, -0.82em -0.09em 0 -0.477em
+            100%
+                box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em
+        @keyframes load6
+            0%
+                box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em
+            5%, 95%
+                box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em
+            10%, 59%
+                box-shadow: 0 -0.83em 0 -0.4em, -0.087em -0.825em 0 -0.42em, -0.173em -0.812em 0 -0.44em, -0.256em -0.789em 0 -0.46em, -0.297em -0.775em 0 -0.477em
+            20%
+                box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em, -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em, -0.749em -0.34em 0 -0.477em
+            38%
+                box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em, -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em, -0.82em -0.09em 0 -0.477em
+            100%
+                box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em
+        @-webkit-keyframes round
+            0%
+                -webkit-transform: rotate(0deg)
+                transform: rotate(0deg)
+            100%
+                -webkit-transform: rotate(360deg)
+                transform: rotate(360deg)
+        @keyframes round
+            0%
+                -webkit-transform: rotate(0deg)
+                transform: rotate(0deg)
+            100%
+                -webkit-transform: rotate(360deg)
+                transform: rotate(360deg)
+    '''  # noqa
+
+    def py2js(self):
+        up.proxy.config.slowDelay = 25
+        up.compiler('.SpinnerOverlay', lambda element: [
+            up.on('up:proxy:slow', lambda: up.element.show(element)),
+            up.on('up:proxy:recover', lambda: up.element.hide(element)),
+        ])
+
+    def __init__(self):
+        super().__init__(Div('...', cls='loader'), style='display:none')
+
+
 class App(Html):
     body_class = Body
     scripts = [
@@ -177,7 +262,7 @@ class App(Html):
         )
         if title := getattr(context['view'], 'title', None):
             self.head.content.append(Title(title))
-        return super().to_html(*content, **context)
+        return super().to_html(*content, SpinnerOverlay(), **context)
 
 
 class NarrowCard(Div):
